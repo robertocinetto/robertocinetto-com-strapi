9bce1cc659b2f5083f44b9f8ce1f8b46430fe20b
Roberto Cinetto <roberto.cinetto@gmail.com>
2022-10-31 18:04:38 -0700

    Fix: production deploy fix

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,8 @@
+// path: ./config/env/production/database.js
+
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
